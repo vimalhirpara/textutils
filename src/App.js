261223dd315036010baf6178cae3afe297c117b5
vimalhirpara <vimalhirpara9597@gmail.com>
2024-06-25 23:37:43 +0530

    added about us component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -9,6 +10,7 @@ function App()
       <Navbar title="Text Utils" about="About" />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze" />
+        {/* <About /> */}
       </div>
     </>
   );
